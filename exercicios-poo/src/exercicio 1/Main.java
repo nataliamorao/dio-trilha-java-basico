93ab3667aa20867d000367d55a74a6af6feb1278
nataliamorao <natalia.morao32@gmail.com>
2025-03-03 11:24:07 -0300

    exercicio 3

--- exercicios-poo/src/exercicio 1/Main.java
+++ exercicios-poo/src/exercicio 1/Main.java
@@ -1,3 +1,5 @@
+package exercicio1;
+
 import java.util.Scanner;
 
 public class Main {
--- exercicios-poo/src/exercicio 1/Pessoa.java
+++ exercicios-poo/src/exercicio 1/Pessoa.java
@@ -1,3 +1,5 @@
+package exercicio1;
+
 public class Pessoa {
     private String nome;
     private int anoNascimento;
