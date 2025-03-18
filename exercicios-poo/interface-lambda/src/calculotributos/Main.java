3f71cf6cb7f7ff3f0bedecafc8ae47b5ed49794b
nataliamorao <natalia.morao32@gmail.com>
2025-03-18 20:16:59 -0300

    adicionando desafio

--- exercicios-poo/interface-lambda/src/calculotributos/Main.java
+++ exercicios-poo/interface-lambda/src/calculotributos/Main.java
@@ -1,24 +1,30 @@
 package calculotributos;
 
+import calculotributos.CalculadoraTributos;
+import calculotributos.ProdutoAlimentacao;
+import calculotributos.ProdutoSaudeBemEstar;
+import calculotributos.ProdutoVestuario;
+import calculotributos.ProdutoCultura;
+
 public class Main {
-  public static void main(String[] args) {
-      CalculadoraTributos calculadora = new CalculadoraTributos();
+    public static void main(String[] args) {
+        CalculadoraTributos calculadora = new CalculadoraTributos();
 
-      Tributavel alimentacao = valor -> valor * 0.01;
-      Tributavel saudeBemEstar = valor -> valor * 0.015;
-      Tributavel vestuario = valor -> valor * 0.025;
-      Tributavel cultura = valor -> valor * 0.04;
+        ProdutoAlimentacao alimentacao = new ProdutoAlimentacao();
+        ProdutoSaudeBemEstar saudeBemEstar = new ProdutoSaudeBemEstar();
+        ProdutoVestuario vestuario = new ProdutoVestuario();
+        ProdutoCultura cultura = new ProdutoCultura();
 
-      double valorProduto = 100.0;
+        double valorProduto = 100.0;
 
-      double tributoAlimentacao = calculadora.calcularTributo(alimentacao, valorProduto);
-      double tributoSaudeBemEstar = calculadora.calcularTributo(saudeBemEstar, valorProduto);
-      double tributoVestuario = calculadora.calcularTributo(vestuario, valorProduto);
-      double tributoCultura = calculadora.calcularTributo(cultura, valorProduto);
+        double tributoAlimentacao = calculadora.calcularTributo(alimentacao, valorProduto);
+        double tributoSaudeBemEstar = calculadora.calcularTributo(saudeBemEstar, valorProduto);
+        double tributoVestuario = calculadora.calcularTributo(vestuario, valorProduto);
+        double tributoCultura = calculadora.calcularTributo(cultura, valorProduto);
 
-      System.out.println("Tributo Alimentação: R$" + tributoAlimentacao);
-      System.out.println("Tributo Saúde e Bem-estar: R$" + tributoSaudeBemEstar);
-      System.out.println("Tributo Vestuário: R$" + tributoVestuario);
-      System.out.println("Tributo Cultura: R$" + tributoCultura);
-  }
+        System.out.println("Tributo Alimentação: R$" + tributoAlimentacao);
+        System.out.println("Tributo Saúde e Bem-estar: R$" + tributoSaudeBemEstar);
+        System.out.println("Tributo Vestuário: R$" + tributoVestuario);
+        System.out.println("Tributo Cultura: R$" + tributoCultura);
+    }
 }
---
+++ poo-desafio/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+      iPhone meuiPhone = new iPhone();
+
+      meuiPhone.tocar();
+      meuiPhone.ligar("123456789");
+      meuiPhone.exibirPagina("www.google.com");
+  }
+}
